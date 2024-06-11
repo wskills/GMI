ac087eff476b735a05afa6c90a9836540a6d44ce
wskills <wskills.solution@gmail.com>
2024-06-11 09:30:13 +0800

    day 1

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("helloWorld123123123")
+
+# print("Hello\n\n")
+# print("Hello1")
+# print("hello2",end="--")
+# print("Hello3")
+# print("\"hello\"")
+# print("helloA","helloB","helloC",sep="\t")
+# this is a single line comment
+
+#literal
+# print("2")
+# print(2)
+# print(2.0)
+# print(.2)
+# print(0o10)  #number in octal
+# print(0xf)    #number in hexadecimal
+# print(0.0005)
+# print(0.00005)
+# print(0.000005)
+
+#arithmetic operators
+print(5+5.0)
+print(5-5)
+print("multiply",5*5,sep="-->")
+print("division",5/5,sep="-->")
+
+print("division",10/3,sep="-->")
+print("integer division",10//3,sep="-->")
+
+print("exponentiation",5**5,sep="-->")
+print("modulo",10%3,sep="-->")
+
+print(-1+5*6**2/2)
